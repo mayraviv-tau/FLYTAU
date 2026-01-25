2e3b3fff213ee5292727d304ebbf356150ae07a2
Amit Hovav <hovavamit@gmail.com>
2026-01-25 15:17:19 +0200

    local server bug fixes

--- app/main.py
+++ app/main.py
@@ -13,6 +13,6 @@ app = create_app()
 
 if __name__ == '__main__':
     print("Starting FLYTAU server...")
-    print("Local URL: http://localhost:5000")
+    print("Local URL: http://localhost:5001")
     print("Make sure MySQL is running with database 'flytau'")
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5001)
